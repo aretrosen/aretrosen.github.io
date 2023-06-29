143478b449f76eabc2b261145b4854d36a24c1d3
Aritra Sen <aretrosen@proton.me>
2023-06-29 06:48:08 +0530

    (feat) Copied and pasted from astro.build, LOL

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://aretrosen.github.io",
   integrations: [tailwind(), mdx(), compress(), robotsTxt(), sitemap()],
   experimental: { assets: true },
 });
